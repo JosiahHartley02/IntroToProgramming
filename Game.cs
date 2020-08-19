c5d831afe8bf4c6df50876bd8e2d44cf06ff2ffe
Josiah Hartley <josiah.t.hartley@gmail.com>
2020-08-19 12:04:55 -0500

    Cleaned Up Code Slightly

--- Game.cs
+++ Game.cs
@@ -21,21 +21,25 @@ namespace HelloWorld
             Console.WriteLine("Cashier: Hello and welcome to Mcdonald what is your name?");
             //Using Console.Readline() works as a wait command
             string name = Console.ReadLine();
-            Console.WriteLine("Cashier: Alright " + name + "what you want to eat?");
+            Console.WriteLine("Cashier: Alright " + name + " what you want to eat?");
             string food = Console.ReadLine();
-            Console.WriteLine("Cashier: Aight I gotchu one " + food + "give me a minute, read this while you wait.");
-            Console.WriteLine("Press any key");
+            Console.WriteLine("Cashier: Aight I gotchu one " + food + " give me a minute, read this while you wait.");
+            Console.WriteLine("press ENTER to continue");
             Console.ReadLine();
             Console.WriteLine(name + "'s health is currently " + health + " don't ask why you have health");            
-            Console.WriteLine("You ordered" + food);
+            Console.WriteLine("You ordered " + food);
             Console.WriteLine("You are now $4.56 lighter");
+            Console.WriteLine("press ENTER to continue");
             Console.ReadLine();
             Console.WriteLine("Here's your" + food);
+            Console.WriteLine("press ENTER key to eat");
             Console.ReadLine();
-            Console.WriteLine("press any key to eat");
-            Console.ReadLine();
+            Console.WriteLine("WARNING: You have leveled up to food poisoning lvl 1");
             Console.WriteLine("You took " + foodpoisoning + " damage!");
-            Console.WriteLine("WARNING: You have food poisoning");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("You have learned a new level of foodpoisoning");
+            Console.WriteLine(name + " is now level " + level + " of food poisoning");
             Console.ReadLine();
 
 
